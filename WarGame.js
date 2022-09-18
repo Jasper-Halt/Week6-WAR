06e8b759cec04a6c747018d682616479dc125ea6
Jasper-Halt <jphalt@aol.com>
2022-09-17 19:55:34 -0600

    updated comments

--- WarGame.js
+++ WarGame.js
@@ -9,11 +9,11 @@ compare each card; one point to whichever player has the higher value (basic Boo
 final score/declared winner at end of array
 preferred: running score tally as game progresses
 */
-const suit = ["Hearts", "Diamonds", "Spades", "Clubs"];
+const suit = ["Hearts", "Diamonds", "Spades", "Clubs"]; //establishes suits, ranks, and values for the cards.
 const rank = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
 const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-class Card {
+class Card {                              //creates card class, with suits, ranks, and values for each individual card in the deck.
     constructor(rank, suit, value) {
         this.rank = rank;
         this.suit = suit;
@@ -21,7 +21,7 @@ class Card {
     }
 }
 
-class Player {
+class Player {                        //creates player class, establishing that each player will have a name, a hand (an array), and a score that starts at 0.
     constructor(name) {
         this.name = name;
         this.hand = [];
@@ -29,27 +29,29 @@ class Player {
     }
 }
 
-class Deck {
+class Deck {                       //creates the class for the deck of cards
     constructor() {
-        this.deck = [];
+        this.deck = [];          //an empty array to hold the cards
     }
     createDeck() {
-        for (let i = 0; i < rank.length; i++) {
-            for (let x = 0; x < suit.length; x++) {
-                this.deck.push(new Card(rank[i], suit[x], value[i]))
+        for (let i = 0; i < rank.length; i++) {                //for loop iterates through the "rank" array
+            for (let x = 0; x < suit.length; x++) {            //nested for loop iterates through the "suit" array, resulting in 52 cards.
+                this.deck.push(new Card(rank[i], suit[x], value[i]))  /*pushes the cards to the empty "this.deck" array. By using the same variable (i) for
+                                                                       both "rank" and "value," it ties those together, resulting in consistent values for all
+                                                                       52 cards in the deck. */
             }
         let shuffledDeck = [];
-        shuffledDeck = this.deck.sort(() => Math.random() - 0.5);
+        shuffledDeck = this.deck.sort(() => Math.random() - 0.5);   //an array sort method with a math.random command shuffles the deck.
         }
     }
 
-    dealDeck() {
+    dealDeck() {                                      //deal deck function uses the slice method to cut the deck in half, distributing 26 cards to each player.
         player1.hand = this.deck.slice(0,26);
         player2.hand = this.deck.slice(26,52);
     }
 }
 
-function compareTest(x, y) {
+function compareTest(x, y) {              //a test function, to check my reasoning that if/else statements will work for the game logic.
     if (x > y) {
         return "X wins!";
     } else if (x < y) {
@@ -57,21 +59,21 @@ function compareTest(x, y) {
     } else return "Tie!";
     }
 
-let player1 = new Player("Player");
+let player1 = new Player("Player");        //creates the two Players ("Player" and "Computer"), instantiating the Player class.
 let player2 = new Player("Computer");
 
-let freshDeck = new Deck();
+let freshDeck = new Deck();              //instantiates the deck class, and calls the functions for creating and dealing the deck.
 freshDeck.createDeck()
 freshDeck.dealDeck();
 
-class Game {
+class Game {                          //creates the class for the actual game (bizarrely, this was much easier than creating the card deck.)
     constructor() {
 
     }
     compareCards() {
-        for (let round = 0; round < 26; round++) {
-            if (player1.hand[round].value > player2.hand[round].value) {
-                player1.score = player1.score + 1;
+        for (let round = 0; round < 26; round++) {                        //for loop iteratest through the 26 rounds of the game.
+            if (player1.hand[round].value > player2.hand[round].value) {   //if statement for if Player 1 has the larger value for the round.
+                player1.score = player1.score + 1;                        //adds one to Player 1's score if the condition above was met.
                 console.log(`
     ROUND ${round}:
 
@@ -82,9 +84,9 @@ class Game {
 
     SCORE:
     PLAYER: ${player1.score}.    COMPUTER: ${player2.score}.
-                `);
-            } else if (player2.hand[round].value > player1.hand[round].value) {
-                player2.score = player2.score + 1;
+                `);                                                    //Object literal, making for easy control of how the round displays in the console.
+            } else if (player2.hand[round].value > player1.hand[round].value) {   //if statement for if Player 2 has the larger value for the round.
+                player2.score = player2.score + 1;                                //adds one to Player 2's score if condition above was met.
                 console.log(`
                 ROUND ${round}:
 
@@ -95,8 +97,8 @@ class Game {
 
     SCORE:
     PLAYER: ${player1.score}.    COMPUTER: ${player2.score}.            
-                `);
-            } else {
+                `);                                                 
+            } else {                                                   //"Else" statement for if the round ends in a tie.
                 console.log(`
     ROUND ${round}:
     
@@ -110,7 +112,7 @@ class Game {
     `);
             }
         }
-        if (player1.score > player2.score) {
+        if (player1.score > player2.score) {                           //If/Else statement calls on the final iteration of "Score" to determine the winner.
             console.log(`
     PLAYER wins with a final score of ${player1.score} points.
 
@@ -132,17 +134,5 @@ class Game {
         }
     }
 
-let newGame = new Game();
+let newGame = new Game();                                          //creates new game and calls the "compareCards" function that controls the game.
 newGame.compareCards();
-/*deck is now shuffled and dealt, so that's big progress.
-All that remains is the actual game:
-1. Compare values between Player 1 and Player 2 arrays.
-    If/else statements (If player1.hand.value[i] > player2.hand.value[i], increase player1's score by 1;
-    else if player1.hand.value[i] < player2.hand.value[i], increase player 2's score by 1;
-    else a tie, so no score change.)
-2. Do unit testing (mocha/chai) on the comparison loop.
-3. Loop through the arrays, so the comparison happens to all 26 cards, with each round printed to console.  Make the console.log a part of the loop.
-3. declare a winner based on the final score values, with the result printed to console.
-
-Probably just print it to the console for simplicity's sake.  It's already late, so I'd as soon be done with it.
-*/
